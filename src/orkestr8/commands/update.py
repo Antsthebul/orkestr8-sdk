e2d7efbbac0ee6fa94300aa8ddc90e05da643548
antsthebul <anthony.allen.srt@gmail.com>
2024-10-05 20:25:12 -0400

    update git ignore

--- src/orkestr8/commands/update.py
+++ src/orkestr8/commands/update.py
@@ -2,6 +2,7 @@ from orkestr8.clients.data_lake_client import DataLakeClient
 import os
 
 AWS_BUCKET_NAME = os.environ['AWS_BUCKET_NAME']
+PROJECT_LOCATION = os.environ['PROJECT_LOCATION']
 
 def run():
     """Pulls down data from repo"""
