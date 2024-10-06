c03791744aeafdd5f7d781740b783f16903f2ad9
antsthebul <anthony.allen.srt@gmail.com>
2024-10-06 18:50:54 -0400

    Update variables,A GAIN

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,5 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def set_mock_env_variables():
-    for r in ["AWS_SECRET_KEY", "AWS_ACCESS_KEY"]:
+    for r in ["AWS_SECRET_KEY", "AWS_ACCESS_KEY", "AWS_BUCKET_NAME"]:
         os.environ[r] = ""
