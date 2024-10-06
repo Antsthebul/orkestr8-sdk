b46b33770784ae8f4feff9b01a56f70cb3e1b39a
antsthebul <anthony.allen.srt@gmail.com>
2024-10-05 22:30:15 -0400

    Update pre-commit

--- src/orkestr8/commands/update.py
+++ src/orkestr8/commands/update.py
@@ -1,9 +1,30 @@
-from orkestr8.clients.data_lake_client import DataLakeClient
 import os
+from uuid import uuid4
 
-AWS_BUCKET_NAME = os.environ['AWS_BUCKET_NAME']
-PROJECT_LOCATION = os.environ['PROJECT_LOCATION']
+from orkestr8.clients.data_lake_client import DataLakeClient
 
-def run():
+
+def run(args):
     """Pulls down data from repo"""
-    DataLakeClient("s3",AWS_BUCKET_NAME)
+    AWS_BUCKET_NAME = os.environ["AWS_BUCKET_NAME"]
+
+    remote_path, dest_path = args.update.remote_file_path, args.update.dest_file_path
+
+    cl = DataLakeClient("s3", AWS_BUCKET_NAME)
+    if not args.default_yes:
+        confirm = input(
+            "Update is a desctructive operation. The path will be completely overwritten ['Enter y to continue']. "
+        )
+        if confirm != "y":
+            print("Exiting..")
+            return
+
+    name = uuid4()
+    try:
+        os.rename(dest_path, name)
+        cl.get_object(remote_path, dest_path)
+    except Exception as e:
+        print(f"Failed to perform update operatoin due to {type(e)}:{str(e)}")
+    finally:
+        os.removedirs(dest_path)
+    print("Successfully updated")
--- src/orkestr8/main.py
+++ src/orkestr8/main.py
@@ -1,49 +1,46 @@
-from argparse import ArgumentParser
 import os
-import dotenv 
 from enum import Enum
-from orkestr8.commands import update 
+
+import dotenv
+
+from orkestr8.cli import parse_args
+from orkestr8.commands import update
+
 
 class Dispatch(Enum):
     TRAIN = "train"
     RUN = "run"
     UPDATE = "update"
 
+
 dotenv.load_dotenv()
 
-def parse_args():
-    parser = ArgumentParser(prog="Orchkestr8 ML train runner")
-    subparsers = parser.add_subparsers(dest="command",help="Invocation commands")
-    train_parser = subparsers.add_parser("train" ,help="Runs the training logic only")
-    train_parser.add_argument("model_module", action="store", help="The module that contains the model to run. Module MUST have a `run` method defined")
-    subparsers.add_parser("run", help="Runs the data update and training logic")
-    subparsers.add_parser("update", help="Runs the data update function.")
-    subparsers.add_parser("stop", help="Writes to a file")
-    parser.add_argument("--aws-access-key")
-    parser.add_argument("--aws-secret-key")
-    parser.add_argument("--aws-bucket-name")
 
-    return parser.parse_args()
-
-def check_env_variables():
+def check_env_variables(args):
     required_variables = ["AWS_ACCESS_KEY", "AWS_SECRET_KEY"]
 
     for v in required_variables:
         if not os.environ.get(v):
-            raise RuntimeError(f"Improper configuration. '{v}' is not set")
-        
-def run(raw_command:Dispatch):
-    command  = Dispatch(raw_command)
+            attr = getattr(args, v.lower(), None)
+            if attr is None:
+                raise RuntimeError(f"Improper configuration. '{v}' is not set")
+            else:
+                os.environ[v] = attr
+
+
+def run(args):
+    command = Dispatch(args.command)
     if command == Dispatch.TRAIN:
         pass
     elif command == Dispatch.UPDATE:
-        update.run()
-         
+        update.run(args)
+
+
 def main():
     args = parse_args()
-    check_env_variables()
-    print("Ye args => ", args)
-    run(args.command)
+    check_env_variables(args)
+    run(args)
+
 
 if __name__ == "__main__":
     main()
