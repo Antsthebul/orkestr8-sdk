a25f8a6c6e62108303d28ca42db26b36065c80ce
antsthebul <anthony.allen.srt@gmail.com>
2024-10-13 11:51:03 -0400

    Create true shell

--- src/orkestr8/installer.py
+++ src/orkestr8/installer.py
@@ -10,7 +10,7 @@ logger = logging.getLogger()
 def install():
     with open("log.txt", "w") as f:
         logger.info("Installing foodenie_ml..")
-        subprocess.run(["tar -xvzf foodenie_ml.tar.gz"], stdout=f)
+        subprocess.run(["tar -xvzf foodenie_ml.tar.gz"], shell=True, stdout=f)
         subprocess.run(
             [
                 "cd foodenie_ml && python3 pip install -r requirements.txt && rm ../foodenie_ml.tar.gz"
