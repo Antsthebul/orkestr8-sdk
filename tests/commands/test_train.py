6a3f1d8371a5318a9dc50921495edd263f1a0d17
antsthebul <anthony.allen.srt@gmail.com>
2024-12-23 22:32:59 -0500

    Bump version

--- tests/commands/test_train.py
+++ tests/commands/test_train.py
@@ -6,17 +6,16 @@ from orkestr8.commands.train import TrainCommand
 
 class TestTrainCommand:
     @patch("orkestr8.commands.train.start_q", return_value=None)
-    @patch("orkestr8.commands.train.get_pid_save_location")
     @patch("orkestr8.commands.train.importlib.import_module")
     def test_run_command_run_executes_successfully(
-        self, mock_import_module, mock_get_pid_file, mock_start_q, tmp_path
+        self, mock_import_module, mock_start_q, tmp_path
     ):
         # ARRANGE
         file_location = tmp_path / "run_id.txt"
-        mock_get_pid_file.return_value = file_location
 
         # ACT
-        TrainCommand(Mock())._run()
+        with patch("orkestr8.commands.train.PID_FILE_LOCATION", file_location):
+            TrainCommand(Mock())._run()
 
         # ASSERT
         with open(str(file_location)) as f:
